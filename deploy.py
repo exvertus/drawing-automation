5215a1d62251d598756ec31d160239443a6184be
tflanigan <tflanig1@gmail.com>
2022-09-13 22:47:35 -0500

    update to deploy script

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import shlex
 import subprocess
 from os import getenv
@@ -7,21 +8,34 @@ DEPLOYER_SA = getenv('GCP_FUNCTION_DEPLOYER_SA')
 DEPLOYER_SA_KEY = getenv('GCP_FUNCTION_DEPLOYER_KEY')
 RUNTIME = 'python310'
 REGION = 'us-central1'
-SA_CMD = "gcloud auth activate-service-account "\
-    f"{DEPLOYER_SA} "
+SA_LOGIN_CMD = "gcloud auth activate-service-account "\
+    f"{DEPLOYER_SA} --key-file={DEPLOYER_SA_KEY}"
 DEPLOY_CMD = "gcloud functions deploy process-public-images "\
     f"--gen2 --region={REGION} --runtime={RUNTIME} --quiet "\
     "--source=. --entry-point=process_public_images"
+SA_LOGOUT_CMD = "gcloud auth revoke "\
+    f"{DEPLOYER_SA}"
 
-def login_as_service_account(key_file):
-    # TODO: finish this
-    pass
+def login_as_service_account():
+    command = SA_LOGIN_CMD
+    logging.info(command)
+    args = shlex.split(command)
+    return subprocess.run(args, shell=True, check=True, capture_output=True)
+
+def logout():
+    command = SA_LOGOUT_CMD
+    logging.info(command)
+    args = shlex.split(command)
+    return subprocess.run(args, shell=True, check=True, capture_output=True)
 
 def deploy(trigger_bucket):
+    login_as_service_account()
     command = f"{DEPLOY_CMD} --trigger-bucket={trigger_bucket}"
-    print(command)
+    logging.info(command)
     args = shlex.split(command)
-    return subprocess.run(args, shell=True, capture_output=True, check=True)
+    result = subprocess.run(args, shell=True, capture_output=True, check=True)
+    logout()
+    return result
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Deploy GCP Function')
