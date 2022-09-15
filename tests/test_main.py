a7bbff7f0c961ce125d991d4835c3d2401ec9c48
tflanigan <tflanig1@gmail.com>
2022-09-15 14:20:43 -0500

    setup, teardown, and broken tests against functions-framework

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,79 @@
+import datetime
+import main
 import pytest
+import shlex
+import subprocess
+import requests
 import time
 from pathlib import Path
 from os import getenv
 from uuid import uuid4
 from google.cloud import storage
-import main
+
 import deploy
 
 PROJECT = getenv('GCP_PROJECT')
 TEST_INPUT_BUCKET = getenv('TEST_INPUT_BUCKET')
 TEST_OUTPUT_BUCKET = getenv('TEST_OUTPUT_BUCKET')
+LOCAL_PORT = 8099
+LOCAL_URL = f"http://localhost:{LOCAL_PORT}/"
 
 class TestUnit:
     pass
 
-class TestIntegration:
-    pass
+class TestIntegrationFuncFW:
+    """'Narrow' itegration tests 
+    running against functions-framework dependency.
+    """
+    @pytest.fixture(scope='class', autouse=True)
+    def init_functions_framework(self):
+        ff_cmd = "functions-framework --target process_public_images "\
+            f"--signature-type event --port {LOCAL_PORT}"
+        args = shlex.split(ff_cmd)
+        ff_process = subprocess.Popen(
+            args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            cwd=Path(__file__).parents[1])
+
+        yield ff_process
+
+        ff_process.terminate()
+    
+    @pytest.fixture(scope='class')
+    def mounted_session(self):
+        retry_policy = requests.packages.urllib3.util.retry.Retry(
+            total=6, backoff_factor=1)
+        retry_adapter = requests.adapters.HTTPAdapter(
+            max_retries=retry_policy)
+        session = requests.Session()
+        session.mount(LOCAL_URL, retry_adapter)
+        return session
+
+    @pytest.fixture()
+    def trigger_event(self, 
+        init_functions_framework, mounted_session):
+        """Make sure downstream image storage upload method is called.
+        """
+        image_name = f"{uuid4()}.jpg"
+        test_tz = datetime.datetime.now().isoformat()
+        gcp_storage_msg = {'data': {
+            'name': image_name,
+            'bucket': 'output_bucket',
+            'metageneration': '1',
+            'timeCreated': test_tz,
+            'updated': test_tz
+            }
+        }
+        response = mounted_session.post(LOCAL_URL, json=gcp_storage_msg)
+        return {"img": image_name, "response": response}
+
+    # TODO: Fix this broken test once you've settled on correct gcp_storage_msg.
+    def test_expected_response(self, trigger_event):
+        assert trigger_event['response'].ok
+
+    def test_downstream_image_uploaded(self, trigger_event):
+        # TODO: finish test
+        assert "mocked gcp storage method 'upload' for output bucket" == \
+            "called with expected values"
 
 class TestSystem:
     """Tests that run against live system.
@@ -47,7 +105,7 @@ class TestSystem:
         # Use random name per-test run in case cleanup fails.
         # Re-using the same image name will break future test
         # runs in the case that the test was halted before cleanup/delete.
-        image_name = "{}.jpg".format(uuid4())
+        image_name = f"{uuid4()}.jpg"
         blob = input_bucket.blob(image_name)
         blob.upload_from_filename(Path("./tests/test.jpg"))
         # Wait for Function call from upload to complete.
